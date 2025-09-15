f1ed52decad3fccfc6be05aaaaef402f17bae269
dediiswadi <30863623+dediiswadi@users.noreply.github.com>
2025-09-15 11:54:47 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 
 export default defineConfig({
   output: "static",
-  adapter: github(),
   site: process.env.SITE || undefined,
   base: process.env.BASE || undefined,
 });
